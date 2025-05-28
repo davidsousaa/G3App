0004b83150382a08d921e7a449f0e1a3a783964d
afonsoabc36 <silva.af36@gmail.com>
2025-05-28 21:48:29 +0100

    feat: complete lane occupation statistics and cleanup

--- src/main/java/pt/uminho/npr/g3app/FogNodeApp.java
+++ src/main/java/pt/uminho/npr/g3app/FogNodeApp.java
@@ -3,7 +3,6 @@ package pt.uminho.npr.g3app;
 import java.util.List;
 import javax.annotation.Nonnull;
 import org.eclipse.mosaic.rti.DATA;
-import org.apache.commons.lang3.tuple.Pair;
 import java.util.concurrent.ConcurrentHashMap;
 import org.eclipse.mosaic.lib.util.scheduling.Event;
 import org.eclipse.mosaic.lib.objects.traffic.SumoTraciResult;
@@ -20,7 +19,7 @@ import org.eclipse.mosaic.fed.application.ambassador.simulation.communication.Ce
 public class FogNodeApp extends AbstractApplication<ServerOperatingSystem>
         implements CommunicationApplication, MosaicApplication {
 
-    private ConcurrentHashMap<String, List<Pair<String, Long>>> laneOccupation = new ConcurrentHashMap<String, List<Pair<String, Long>>>();
+    private ConcurrentHashMap<String, ConcurrentHashMap<String, Long>> laneOccupation = new ConcurrentHashMap<String, ConcurrentHashMap<String, Long>>();
 
     @Override
     public void onStartup() {
@@ -29,6 +28,7 @@ public class FogNodeApp extends AbstractApplication<ServerOperatingSystem>
                 .maxUplinkBitrate(50 * DATA.MEGABIT));
 
         getLog().infoSimTime(this, "Fog Node started and ready to receive RSU interactions.");
+        getOs().getEventManager().scheduledIn(1_000, "laneInfoCleanup");
     }
 
     @Override
@@ -38,7 +38,26 @@ public class FogNodeApp extends AbstractApplication<ServerOperatingSystem>
 
     @Override
     public void processEvent(Event event) {
-        // No scheduled events in this example
+        if ("laneInfoCleanup".equals(event.getTag())) {
+            cleanupLaneInfo();
+
+            getOs().getEventManager().scheduleIn(1_000, "laneInfoCleanup");
+        }
+    }
+
+    public void cleanupLaneInfo() {
+        long currentTime = getOs().getSimulationTime();
+
+        for (ConcurrentHashMap.Entry<String, ConcurrentHashMap<String, Long>> entry : laneOccupation.entrySet()) {
+            ConcurrentHashMap<String, Long> vehiclesInLane = entry.getValue();
+
+            for (ConcurrentHashMap.Entry<String, Long> vehicleEntry : vehiclesInLane.entrySet()) {
+                long timestamp = vehicleEntry.getValue();
+                if ((currentTime - timestamp) > 1000) {
+                    vehiclesInLane.remove(vehicleEntry.getKey());
+                }
+            }
+        }
     }
 
     @Override
@@ -86,52 +105,47 @@ public class FogNodeApp extends AbstractApplication<ServerOperatingSystem>
 
     private void processMessage(RsuFogInteraction message) {
         String content = message.getContent();
-        String messageType = content.split("\\{")[0];
+        String messageType = content.split("\\{")[0].strip();
         switch (messageType) {
             case "VehInfoMessage":
-                this.processStatusMessage(content);
+                processStatusMessage(content);
                 break;
             case "WarningMessage":
-                this.processWarningMessage(content);
+                processWarningMessage(content);
                 break;
             default:
                 break;
         }
     }
 
-    /*
-     * public String toString() {
-     * return "VehInfoMessage{" +
-     * "timeStamp=" + timeStamp +
-     * ", senderName=" + senderName +
-     * ", senderPosition=" + senderPos +
-     * ", senderHeading=" + senderHeading +
-     * ", senderSpeed=" + senderSpeed +
-     * ", senderLaneId=" + senderLaneId + '}';
-     * }
-     */
-
     private void processStatusMessage(String message) {
-        // TODO: Check if it need more fields
-        String laneId = message.split("senderLaneId=")[1].split("\\}")[0];
-        String vehicleId = message.split("senderName=")[1].split(",")[0];
-        String timestampString = message.split("timeStamp=")[1].split(",")[0];
+        String laneId = message.split("senderLaneId=")[1].split("\\}")[0].strip();
+        String vehicleId = message.split("senderName=")[1].split(",")[0].strip();
+        String timestampString = message.split("timeStamp=")[1].split(",")[0].strip();
         long timestamp = Long.parseLong(timestampString);
-        Pair<String, Long> vehichleTimestamp = Pair.of(vehicleId, timestamp);
 
-        List<Pair<String, Long>> laneInfo = this.laneOccupation.get(laneId);
-        for (Pair<String, Long> pair : laneInfo) {
-            if (pair.getLeft().equals(vehicleId)) {
-                laneInfo.remove(pair);
-                break;
+        // Update timestamp value of the vehicle
+        for (ConcurrentHashMap.Entry<String, ConcurrentHashMap<String, Long>> entry : laneOccupation.entrySet()) {
+            String currentLaneId = entry.getKey();
+            ConcurrentHashMap<String, Long> vehiclesInLane = entry.getValue();
+
+            if (vehiclesInLane.containsKey(vehicleId)) {
+                if (currentLaneId.equals(laneId)) {
+                    vehiclesInLane.put(vehicleId, timestamp);
+                    return; // No need to check if it is in another lane, it would be caught before when
+                            // added to this new lane
+                } else {
+                    // Vehicle registered in a different lane
+                    vehiclesInLane.remove(vehicleId);
+                    break;
+                }
             }
         }
 
-        laneInfo.add(vehichleTimestamp);
-        this.laneOccupation.put(laneId, laneInfo);
-        // TODO:
-        // Outside this, método para limpar se timestamp (pair.getLeft()) difference
-        // maior que 0.5s ou 1s
+        // If the vehicle was not in the correct lane, add it
+        laneOccupation
+                .computeIfAbsent(laneId, k -> new ConcurrentHashMap<>())
+                .put(vehicleId, timestamp);
     }
 
     private void processWarningMessage(String message) {
